f261951db8fbf5a2373c02b181c544e461d06b21
privatepenguinzero <133494026+privatepenguinzero@users.noreply.github.com>
2026-07-07 10:16:27 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -79,7 +79,7 @@ systemctl enable --force greetd.service
 mkdir -p /etc/skel/.config/systemd/user/graphical-session.target.wants
 ln -s /usr/lib/systemd/user/dms.service /etc/skel/.config/systemd/user/graphical-session.target.wants/
 mkdir -p /etc/skel/.config/niri/
-#cp -rf /ctx/dot_config/niri/config.kdl /etc/skel/.config/niri/
+cp -rf /ctx/dot_config/niri/config.kdl /etc/skel/.config/niri/
 
 # DEV packages
 #dnf -y install cargo evtest git input-remapper libevdev-devel libinput-utils python3-devel
